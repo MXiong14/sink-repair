002ab06a817a68f83820bd15f61f0ab0db0d50b2
Matt <Mattxiong_14@hotmail.com>
2021-06-28 08:40:22 -0500

    first commit

---
+++ src/scripts/Requests.js
@@ -0,0 +1,22 @@
+import { getRequests } from "./dataAccess.js"
+
+export const Requests = () => {
+    const requests = getRequests()
+
+    let html = '<ul>'
+
+    const listItems = requests.map(request => {
+                        return `<li>
+                            ${request.description}
+                            <button class="request_delete"
+                                    id="request--${request.id}">
+                                Delete
+                            </button>
+                        </li>`
+                    })
+
+    html += listItems.join("")
+    html += '</ul>'                
+
+    return html
+}
